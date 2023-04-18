86a2a79e9c4be59e10df13704baa42c6ad197378
Densu3 <barialen600@gmail.com>
2023-04-18 19:13:44 +0500

    base.html

--- spotify_app/urls.py
+++ spotify_app/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('', views.index, name='index'),
+    # path('main/', views.main_page, name='main_page' ),
+    # path('search/<slug:slug>/', views.search, name='search'),
+    # path('sorting/<slug:slug>', views.sorting, name='sorting'),
+    # path('category/<slug:slug>/',views.category_detail, name='category_detail_url'),
+    # path('register/', views.register, name= 'register'),
+    # path('authorisation/', views.authorisation, name= 'authorisation'),
 ]
