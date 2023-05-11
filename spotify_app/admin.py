cc747673d892094afea707cfc1e6da37a4242395
Densu3 <barialen600@gmail.com>
2023-05-11 18:11:33 +0500

    Admin

--- spotify_app/admin.py
+++ spotify_app/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import *
+from .models import Post
+
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+admin.site.register(Post, PostAdmin)
 
 # Register your models here.
 
--- spotify_app/views.py
+++ spotify_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import *
-from .forms import RegisterForm
+from .models import Post
+
+def index(request):
+    news = Post.objects.order_by('-created_at') [:3]
+    return render(request, 'spotify_app/index.html', {'news' : news})
